b3abf5b296dad6a7278dead0aa790efcc8bfa4a7
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-27 16:40:46 -0300

    Card onclick e handleClick pra virar o card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -5,15 +5,18 @@ import './index.css';
 //1) Criei a interface pra usar nos cartões
 interface CardProps {
   card: CardType;
+  handleClick: (id: number) => void;
 }
 
 // 2) const Card: React.FC - Criei o Crad como um componente funcional do React
 // <CardProps> - O componente tem como tipo CardProps
 // ({ card }) - A função Card recebe um objeto card como props
 //  return (<div className={`card... - E retorna um elemento div com a classe card
-const Card: React.FC<CardProps> = ({ card }) => {
+const Card: React.FC<CardProps> = ({ card, handleClick }) => {
   return (
-    <div className={`card ${card.isFlipped ? 'flipped' : ''}`}>
+    <div className={`card ${card.isFlipped ? 'flipped' : ''}`}
+      onClick={() => handleClick(card.id)}
+    >
     {/* Adiciono a class card a div e caso o card esteja virado tb adiciono a class flipped */}
       <div className="card__inner">
         <div className="card__front">
